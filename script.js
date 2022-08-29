a7a105731c9129c8cb617300564bd78cef7e2fc1
NightStrang6r <tsaruk-2015@mail.ru>
2022-08-29 21:15:55 +0300

    💚 Improved generator logic

--- script.js
+++ script.js
@@ -3,12 +3,21 @@ const inputCountEl = document.getElementById("inputCount");
 const inputLengthEl = document.getElementById("inputLength");
 const generateButEl = document.getElementById("generateButton");
 
-generateButEl.addEventListener("click", generatePasswords);
-inputCountEl.addEventListener("keyup", (event) => {checkForLetters(event.target)});
-inputLengthEl.addEventListener("keyup", (event) => {checkForLetters(event.target)});
+generateButEl.addEventListener('click', generatePasswords);
+inputCountEl.addEventListener('keyup', (event) => {checkForLetters(event.target); saveCount(event.target);});
+inputLengthEl.addEventListener('keyup', (event) => {checkForLetters(event.target); saveLength(event.target);});
+document.addEventListener('keyup', (event) => {if(event.code != 'Enter') return; generatePasswords();});
 
 let count;
 let length;
+
+if(localStorage.plength) {
+    inputLengthEl.value = localStorage.plength;
+}
+
+if(localStorage.pcount) {
+    inputCountEl.value = localStorage.pcount;
+}
 
 generatePasswords();
 
@@ -48,23 +57,26 @@ function generatePassword(length) {
 
     let symbols = upper + lower + numbers;
     let password = "";
-    
-    for (var i = 0; i < length; i++) {
-        password += symbols.charAt(rand(symbols.length)); 
+
+    if(length < 6) {
+        for (var i = 0; i < length; i++) {
+            password += symbols.charAt(rand(symbols.length)); 
+        }
+
+        return password;
     }
 
-    if(length >= 6) {
-        /* Ensures that the password contains at least 2 uppercase letters, 2 lowercase letters and 2 numbers  */
-        for (let i = 0; i < 2; i++) {
-            password = replaceAt(password, upper.charAt(rand(upper.length)), rand(password.length));
-        }
-        for (let i = 0; i < 2; i++) {
-            password = replaceAt(password, lower.charAt(rand(lower.length)), rand(password.length));
-        }
-        for (let i = 0; i < 2; i++) {
-            password = replaceAt(password, numbers.charAt(rand(numbers.length)), rand(password.length));
-        }
+    for (var i = 0; i < 2; i++) {
+        password += upper.charAt(rand(upper.length));
+        password += lower.charAt(rand(lower.length));
+        password += numbers.charAt(rand(numbers.length)); 
     }
+
+    for (var i = 0; i < (length - 6); i++) {
+        password += symbols.charAt(rand(symbols.length));
+    }
+
+    password = shuffle(password);
 
     return password;
 }
@@ -73,10 +85,27 @@ function checkForLetters(el) {
     el.value = el.value.replace(/[^\d]/g,'');
 }
 
-function replaceAt(string, replacement, index) {
-    return string.substr(0, index) + replacement + string.substr(index + replacement.length);
-}
-
 function rand(to) {
     return Math.floor(Math.random() * to);
+}
+
+function shuffle(string) {
+    array = string.split('');
+
+    for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+
+    string = array.join('');
+
+    return string;
+}
+
+function saveCount(el) {
+    localStorage.pcount = el.value;
+}
+
+function saveLength(el) {
+    localStorage.plength = el.value;
 }
